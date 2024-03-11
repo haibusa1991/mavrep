2df530a70be2e992dd88d7d66011a486eaf3beb1
Plamen Simeonov <haibusa1991@gmail.com>
2024-03-11 19:28:06 +0200

    updated documentation fo api.base and api.errors

--- api/src/main/java/com/personal/microart/api/errors/FileUploadError.java
+++ api/src/main/java/com/personal/microart/api/errors/FileUploadError.java
@@ -1,16 +1,2 @@
-package com.personal.microart.api.errors;
-
-import lombok.Builder;
-//TODO: Delete this class
-public class FileUploadError extends BaseApiError {
-
-    @Builder
-    public FileUploadError(String message) {
-        super(500, message);
-    }
-
-    @Builder
-    public FileUploadError() {
-        super(500, "Cannot upload file");
-    }
+package com.personal.microart.api.errors;public class FileUploadError {
 }
